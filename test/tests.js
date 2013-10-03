509d7958c445e90cfc3687f432e57e7d5f193d1f
Roy de Bokx <info@debokxict.nl>
2013-10-03 12:25:23 +0200

    moved an initialized testsuite

---
+++ test/tests.js
@@ -0,0 +1,21 @@
+require.config({
+    baseUrl: '../src/'
+});
+
+define([
+        'hhvm'
+    ], function(hhvm) {
+        //Tests Nop
+        test("Nop", function(){
+            //TODO
+            ok(false);
+        })
+        
+        test("Test2", function(){
+            ok(false);
+        });
+        
+        //Tests OPC2
+        //TODO
+    }
+);
