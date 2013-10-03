059cdc57c9588e4c32a9856576bc3ae9012ea488
Roy de Bokx <info@debokxict.nl>
2013-10-03 12:16:26 +0200

    initial commit of instruction files

---
+++ src/lib/instructions/instructions.js
@@ -0,0 +1,63 @@
+define([
+        'lib/hhbc'
+    ], function(hhbc) {
+        return [
+            {name: "LowInvalid", func: null},
+            // 1. Basic instructions
+            {name: "Nop", func: hhbc.nop},
+            {name: "PopC", func: null},
+            {name: "PopV", func: null},
+            {name: "PopR", func: null},
+            {name: "Dup", func: null},
+            {name: "Box", func: null},
+            {name: "Unbox", func: null},
+            {name: "BoxR", func: null},
+            {name: "UnboxR", func: null},
+            
+            // 2. Literal and constant instructions
+            {name: "Null", func: null},
+            {name: "True", func: null},
+            {name: "False", func: null},
+            {name: "NullUninit", func: null},
+            {name: "NewArray", func: null},
+            {name: "NewPackedArray", func: null},
+            {name: "AddElemC", func: null},
+            {name: "AddElemV", func: null},
+            {name: "AddNewElemC", func: null},
+            {name: "AddNewElemV", func: null},
+            {name: "NewCol", func: null},
+            {name: "ColAddElemC", func: null},
+            {name: "ColAddNewElemC", func: null},
+            {name: "Cns", func: null},
+            {name: "CnsE", func: null},
+            {name: "CnsU", func: null},
+            {name: "ClsCns", func: null},
+            {name: "ClsCnsD", func: null},
+            
+            // 3. Operator instructions
+            //TODO
+            // 4. Control flow instructions
+            //TODO
+            // 5. Get instructions
+            //TODO
+            // 6. Isset, Empty and type querying instructions
+            //TODO
+            // 7. Mutator instructions
+            //TODO
+            // 8. Call instructions
+            //TODO
+            // 9. Member operations
+            //TODO
+            // 10. Member instructions
+            //TODO
+            // 11. Iterator instructions
+            //TODO
+            // 12. Include, eval, and define instructions
+            //TODO
+            // 13. Miscellaneous instructions
+            //TODO
+            // 14. Continuation creation and execution
+            //TODO
+            ]
+    }
+);
---
+++ src/lib/instructions/opcodes.js
@@ -0,0 +1,9 @@
+define([
+        'vendor/underscore',
+        'lib/instructions/instructions'
+    ], function(_, instructions) {
+        return _.invert(_.map(instructions, function(index, instr) {
+            return instr.name.toUpper();
+        }));
+    }
+);
