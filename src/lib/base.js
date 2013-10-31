43784c2224e7767cd2cac0619cf8620e6e6ded3c
Roy de Bokx <info@debokxict.nl>
2013-10-31 13:14:33 +0100

    implemented IncDecM

---
+++ src/lib/base.js
@@ -0,0 +1,29 @@
+define([
+        'vendor/underscore'
+    ], function(_) {
+        var Base = function(value) {
+            this.value = value !== undefined ? value : null;
+        };
+        
+        Cell.prototype.valueOf = function() {
+            return this.value;
+        };
+        
+        Cell.prototype.clone = function() {
+            return new Base(this.value);
+        };
+        
+        Cell.prototype.toString = function() {
+            if (this.value === null || this.value === false) {
+                return "";
+            } else if (this.value === true) {
+                return "1";
+            } else if (_.isArray(this.value)) {
+                return "Array";
+            }
+            return "" + this.value;
+        };
+        
+        return Base;
+    }
+);
--- src/lib/instructions/memberInstructions.js
+++ src/lib/instructions/memberInstructions.js
@@ -4,12 +4,86 @@ define([
     ], function(_, subopcodes) {
         return {
             //TODO: implement missing functions
+            
+            
+            
             IncDecM: function(op, vector){
-                //TODO: implement function
-                var mnemonic = subopcodes.IncDec.getMnemonic(op);
-                console.log("IncDecM: mnemonic: " + mnemonic + " vector: " + vector);
+                //retrieve base
+                var base = null;
+                switch(vector.locationDescriptor.type){
+                    case 'C':
+                        base = this.hhbc.BaseC();
+                        break;
+                    case 'R':
+                        base = this.hhbc.BaseR();
+                        break;
+                    case 'L':
+                        base = this.hhbc.BaseLWD(vector.LocationDescriptor.param);
+                        break;
+                    case 'NC':
+                        base = this.hhbc.BaseNCWD();
+                        break;
+                    case 'NL':
+                        base = this.hhbc.BaseNLWD(vector.LocationDescriptor.param);
+                        break;
+                    case 'GC':
+                        base = this.hhbc.BaseGCWD();
+                        break;
+                    case 'GL':
+                        base = this.hhbc.BaseGLWD(vector.LocationDescriptor.param);
+                        break;
+                    case 'SC':
+                        base = this.hhbc.BaseSC();
+                        break;
+                    case 'SL':
+                        base = this.hhbc.BaseSL(vector.LocationDescriptor.param);
+                        break;
+                    case 'H':
+                        base = this.hhbc.BaseH();
+                        break;
+                }
                 
+                //Perform intermediate operations
+                for(var i = 0; i < vector.members.length - 1; i++){
+                    var member = members[i];
+                    switch(member.type){
+                        case 'EC':
+                            this.hhbc.ElemCWD(base);
+                            break;
+                        case 'PC':
+                            this.hhbc.PropCWD(base);
+                            break;
+                        case 'EL':
+                            this.hhbc.ElemLWD(member.value, base);
+                            break;
+                        case 'PL':
+                            this.hhbc.PropLWD(member.value, base);
+                            break;
+                        case 'W':
+                            this.hhbc.NewElem(base);
+                            break;
+                    }
+                }
                 
+                //Perform final operation
+                var lastMember = vector.members[vector.members.length - 1];
+                switch(lastMember.type){
+                    case 'EC':
+                        this.hhbc.IncDecElemC(op, base);
+                        break;
+                    case 'PC':
+                        this.hhbc.IncDecPropC(op, base);
+                        break;
+                    case 'EL':
+                        this.hhbc.IncDecElemL(op, lastMember.value, base);
+                        break;
+                    case 'PL':
+                        this.hhbc.IncDecPropL(op, lastMember.value, base);
+                        break;
+                    case 'W':
+                        this.hhbc.IncDecNewElem(op, base);
+                        break;
+                }
             }
         };
     }
