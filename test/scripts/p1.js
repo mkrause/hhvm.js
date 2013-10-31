c22e771153c78aec6882651dfa8919ade97e336f
Roy de Bokx <info@debokxict.nl>
2013-10-31 09:22:07 +0100

    made server response in p1 to p6 better readable.

--- test/scripts/p1.js
+++ test/scripts/p1.js
@@ -5,6 +5,85 @@ require.config({
 
 define([
     ], function() {
-        return {"units":[{"bc":[16,0,0,0,0,65,2,14,1,0,0,0,0,0,0,0,74],"bc_meta":[],"filepath":"/tmp/script_61b080653803c560f93f1ff58fd22f3028bfef5f","md5":"6789ed8b7547b703de027095819c25ba","linetable":[{"pastOffset":5,"line":2},{"pastOffset":7,"line":2},{"pastOffset":17,"line":2}],"mainReturn":{"type":"Uninit","value":"6"},"mergeable":false,"typedefs":[],"litStrs":["Hello World"],"functions":[{"name":"","fullName":"","line1":1,"line2":2,"base":0,"past":17,"attrs":65664,"returnType":-1,"docComment":"","numLocals":0,"numIterators":0,"maxStackCells":1,"isClosureBody":false,"isGenerator":false,"isGeneratorFromClosure":false,"isPairGenerator":false,"hasGeneratorAsBody":false,"isAsync":false,"params":[],"localNames":[],"staticVars":[],"userAttributes":{},"retTypeConstraint":"","originalFilename":""}],"arrays":[]}]};
+        return {
+            "units": [
+                {
+                    "bc": [
+                        16,
+                        0,
+                        0,
+                        0,
+                        0,
+                        65,
+                        2,
+                        14,
+                        1,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0,
+                        74
+                    ],
+                    "bc_meta": [],
+                    "filepath": "/tmp/script_d0592899a1a080058502ee148879005b1a7c171c",
+                    "md5": "6789ed8b7547b703de027095819c25ba",
+                    "linetable": [
+                        {
+                            "pastOffset": 5,
+                            "line": 2
+                        },
+                        {
+                            "pastOffset": 7,
+                            "line": 2
+                        },
+                        {
+                            "pastOffset": 17,
+                            "line": 2
+                        }
+                    ],
+                    "mainReturn": {
+                        "type": "Uninit",
+                        "value": "6"
+                    },
+                    "mergeable": false,
+                    "typedefs": [],
+                    "litStrs": [
+                        "Hello World"
+                    ],
+                    "functions": [
+                        {
+                            "name": "",
+                            "fullName": "",
+                            "line1": 1,
+                            "line2": 2,
+                            "base": 0,
+                            "past": 17,
+                            "attrs": 65664,
+                            "returnType": -1,
+                            "docComment": "",
+                            "numLocals": 0,
+                            "numIterators": 0,
+                            "maxStackCells": 1,
+                            "isClosureBody": false,
+                            "isGenerator": false,
+                            "isGeneratorFromClosure": false,
+                            "isPairGenerator": false,
+                            "hasGeneratorAsBody": false,
+                            "isAsync": false,
+                            "params": [],
+                            "localNames": [],
+                            "staticVars": [],
+                            "userAttributes": {},
+                            "retTypeConstraint": "",
+                            "originalFilename": ""
+                        }
+                    ],
+                    "arrays": []
+                }
+            ]
+        };
     }
 );
--- test/scripts/p6.js
+++ test/scripts/p6.js
@@ -5,6 +5,67 @@ require.config({
 
 define([
     ], function() {
-        return {"units":[{"bc":[17,0,0,0,0,117,0,2,14,0,0,0,0,0,0,0,0,117,2,2,78,0,13,169,4,2,0,0,0,0,9,79,2,47,70,30,0,0,0,129,2,1,84,3,0,0,0,1,0,0,0,0,0,0,2,129,2,0,2,69,217,255,255,255,78,0,13,169,4,2,1,0,0,0,4,14,1,0,0,0,0,0,0,0,74],"bc_meta":[2,0,0,0,30,0,0,0,33,0,0,0,255,255,255,127,28,0,0,0,31,0,0,0,34,0,0,0,3,0,0,4,0,20],"filepath":"/tmp/script_3e56bdb7dbb714ff866464b22d5fb60aebe42f54","md5":"d2b0c86a603c7ecf45546b9d65c2800a","linetable":[{"pastOffset":7,"line":5},{"pastOffset":8,"line":5},{"pastOffset":17,"line":6},{"pastOffset":19,"line":6},{"pastOffset":20,"line":8},{"pastOffset":31,"line":6},{"pastOffset":39,"line":6},{"pastOffset":42,"line":7},{"pastOffset":55,"line":7},{"pastOffset":58,"line":6},{"pastOffset":64,"line":8},{"pastOffset":74,"line":9},{"pastOffset":75,"line":9},{"pastOffset":85,"line":9}],"mainReturn":{"type":"Uninit","value":"6"},"mergeable":false,"typedefs":[],"litStrs":["sizeof","print_r"],"functions":[{"name":"","fullName":"","line1":1,"line2":9,"base":0,"past":85,"attrs":65664,"returnType":-1,"docComment":"","numLocals":2,"numIterators":0,"maxStackCells":4,"isClosureBody":false,"isGenerator":false,"isGeneratorFromClosure":false,"isPairGenerator":false,"hasGeneratorAsBody":false,"isAsync":false,"params":[],"localNames":["newArray","i"],"staticVars":[],"userAttributes":{},"retTypeConstraint":"","originalFilename":""}],"arrays":[{}]}]};
+        return {
+            "units": [
+                {
+                    "bc": [
+                        16,
+                        0,
+                        0,
+                        0,
+                        0,
+                        68,
+                        0
+                    ],
+                    "bc_meta": [],
+                    "filepath": "/tmp/script_0cd2254e86e4aeb7a0f08d26e8654355c683fb60",
+                    "md5": "d1cad5c3a55f261895101d16ab214356",
+                    "linetable": [
+                        {
+                            "pastOffset": 7,
+                            "line": 8
+                        }
+                    ],
+                    "mainReturn": {
+                        "type": "Uninit",
+                        "value": "6"
+                    },
+                    "mergeable": false,
+                    "typedefs": [],
+                    "litStrs": [
+                        "syntax error, unexpected T_INC"
+                    ],
+                    "functions": [
+                        {
+                            "name": "",
+                            "fullName": "",
+                            "line1": 8,
+                            "line2": 8,
+                            "base": 0,
+                            "past": 7,
+                            "attrs": 65664,
+                            "returnType": -1,
+                            "docComment": "",
+                            "numLocals": 0,
+                            "numIterators": 0,
+                            "maxStackCells": 1,
+                            "isClosureBody": false,
+                            "isGenerator": false,
+                            "isGeneratorFromClosure": false,
+                            "isPairGenerator": false,
+                            "hasGeneratorAsBody": false,
+                            "isAsync": false,
+                            "params": [],
+                            "localNames": [],
+                            "staticVars": [],
+                            "userAttributes": {},
+                            "retTypeConstraint": "",
+                            "originalFilename": ""
+                        }
+                    ],
+                    "arrays": []
+                }
+            ]
+        };
     }
 ); 
